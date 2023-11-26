70e88bb4e54f52f3fb6b30e2f083240cc8d02258
“caixiaodong2002” <1845984065@qq.com>
2023-11-26 13:31:28 +0800

    add cookies

--- src/main/java/com/cxd/community/Controller/IndexController.java
+++ src/main/java/com/cxd/community/Controller/IndexController.java
@@ -1,13 +1,35 @@
 package com.cxd.community.Controller;
 
+import com.cxd.community.mapper.UserMapper;
+import com.cxd.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user =  userMapper.findToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
